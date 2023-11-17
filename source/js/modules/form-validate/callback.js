0fc4e67ab36de9d809fd31860dc1d572c6c6b746
Dmitrii Timofeev <28dngn@gmail.ru>
2023-11-18 00:59:13 +0300

    валидация сделана

--- source/js/modules/form-validate/callback.js
+++ source/js/modules/form-validate/callback.js
@@ -1,7 +1,18 @@
 
 const baseSuccessCallback = (event) => {
   event.preventDefault();
-  // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
+  const url = 'https://echo.htmlacademy.ru/';
+  const formData = new FormData(document.querySelector('[data-form-node] form'));
+
+  fetch(url, {
+    method: 'POST',
+    body: formData,
+  }).then((response) => response.text()).then((data) => {
+    document.body.innerHTML = `<pre>${data}</pre>`;
+  }).catch(() => {
+    document.body.innerHTML = 'Ошибка: не удалось отправить форму на сервер';
+  });
+
 };
 
 const baseErrorCallback = (event) => {
